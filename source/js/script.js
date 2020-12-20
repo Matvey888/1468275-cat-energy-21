608bfcf8e01d0be3d1eaabfc1c2401a828d6c976
Matvey Avdeev <svetoslavhorobriy@gmail.com>
2020-12-20 14:07:03 +0300

    Простоя реализация слайдера по ТЗ

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,9 @@
 let navMain = document.querySelector('.main-nav');
 let navToggle = document.querySelector('.main-nav__toggle');
+let buttonBefore = document.querySelector('.buttons-list__button--before');
+let buttonAfter = document.querySelector('.buttons-list__button--after');
+let imgBefore = document.querySelector('.live-example__image--before');
+let imgAfter = document.querySelector('.live-example__image--after');
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -11,4 +15,14 @@ navToggle.addEventListener('click', function () {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
   }
+});
+
+buttonBefore.addEventListener('click', function (evt) {
+  imgBefore.classList.add('live-example__image--show');
+  imgAfter.classList.remove('live-example__image--show');
+});
+
+buttonAfter.addEventListener('click', function (evt) {
+  imgAfter.classList.add('live-example__image--show');
+  imgBefore.classList.remove('live-example__image--show');
 });
